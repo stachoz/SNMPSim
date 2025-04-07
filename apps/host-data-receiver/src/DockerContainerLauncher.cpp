25445a316b1e2c80c67f9958b491cbcf04663ae4
stachoz <stasiekzdybel9@gmail.com>
2025-04-07 22:37:59 +0200

    refactor

--- apps/host-data-receiver/src/DockerContainerLauncher.cpp
+++ apps/host-data-receiver/src/DockerContainerLauncher.cpp
@@ -2,3 +2,49 @@
 // Created by stachoz on 3/11/25.
 //
 
+
+#include "DockerContainerLauncher.h"
+
+DockerContainerLauncher::DockerContainerLauncher(QObject *parent): QObject(parent) {}
+
+DockerContainerLauncher::~DockerContainerLauncher() {
+    std::cout << "Stopping containers" << std::endl;
+    for(auto& containerHash : containers) {
+        std::string cmd = "docker stop " + containerHash;
+        system(cmd.c_str());
+    }
+}
+
+void DockerContainerLauncher::startDockerContainer() {
+    // TODO run in separate thread
+    std::string container1 = execCommand("docker run -d snmp-server-app:latest");
+    std::string container2 = execCommand("docker run -d snmp-agent:latest");
+    std::string container3 = execCommand("docker run -d snmp-agent:latest");
+    std::string container4 = execCommand("docker run -d snmp-agent-server-device:latest");
+
+    containers.push_back(container1);
+    containers.push_back(container2);
+    containers.push_back(container3);
+    containers.push_back(container4);
+
+    for (auto& conHash : containers) {
+        std::cout << conHash << std::endl;
+    }
+}
+
+std::string DockerContainerLauncher::execCommand(const char *cmd) {
+    std::array<char, 128> buffer {};
+    std::string result;
+
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+
+    if(!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+
+    while(fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+
+    return result;
+}
--- apps/host-data-receiver/src/DockerContainerLauncher.h
+++ apps/host-data-receiver/src/DockerContainerLauncher.h
@@ -4,55 +4,21 @@
 
 #pragma once
 #include <iostream>
+#include <qobject.h>
 #include <string>
 
 class DockerContainerLauncher : public QObject {
     Q_OBJECT
 public:
-    explicit DockerContainerLauncher(QObject* parent = nullptr) : QObject(parent) {};
+    explicit DockerContainerLauncher(QObject* parent = nullptr);;
 
-    ~DockerContainerLauncher() override {
-        for(auto& containerHash : containers) {
-            std::cout << "Stopping containers" << std::endl;
-            std::string cmd = "docker stop " + containerHash;
-            system(cmd.c_str());
-        }
-    };
+    ~DockerContainerLauncher() override;
 
 public slots:
-    void startDockerContainer() {
-        std::string container1 = execCommand("docker run -d snmp-server-app:latest");
-        std::string container2 = execCommand("docker run -d snmp-agent:latest");
-        std::string container3 = execCommand("docker run -d snmp-agent:latest");
-        std::string container4 = execCommand("docker run -d snmp-agent-server-device:latest");
-
-        containers.push_back(container1);
-        containers.push_back(container2);
-        containers.push_back(container3);
-        containers.push_back(container4);
-
-        for (auto& conHash : containers) {
-            std::cout << conHash << std::endl;
-        }
-    }
+    void startDockerContainer();
 
 private:
     std::vector<std::string> containers {};
 
-    std::string execCommand(const char* cmd) {
-        std::array<char, 128> buffer {};
-        std::string result;
-
-        std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-
-        if(!pipe) {
-            throw std::runtime_error("popen() failed!");
-        }
-
-        while(fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-            result += buffer.data();
-        }
-
-        return result;
-    }
+    std::string execCommand(const char* cmd);
 };
